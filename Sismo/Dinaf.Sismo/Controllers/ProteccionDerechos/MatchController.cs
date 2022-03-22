4ae1514c1a51537092705b614d58b496263af650
unknown <maicolknales@gmail.com>
2022-03-22 16:59:06 -0600

    Se agregaron acordiones en pestañas de opcion demanada

---
+++ Sismo/Dinaf.Sismo/Controllers/ProteccionDerechos/MatchController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dinaf.Sismo.Controllers.ProteccionDerechos
+{
+    public class MatchController : Controller
+    {
+        // GET: MatchController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MatchController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MatchController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MatchController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MatchController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MatchController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MatchController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MatchController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
